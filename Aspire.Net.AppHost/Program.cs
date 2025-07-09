37582a677bf196d0952ab75e7020e8e617a0a2ad
Eric Albert Sabino Pinho <eric.uellendahl@gmail.com>
2025-07-09 11:17:35 -0300

    add project references and service defaults integration

--- Aspire.Net.AppHost/Program.cs
+++ Aspire.Net.AppHost/Program.cs
@@ -2,8 +2,11 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var apiService = builder.AddProject<Projects.Aspire_Net_ApiService>("apiservice");
 
+var worker = builder.AddProject<Projects.Apire_Worker>("worker");
+
 builder.AddProject<Projects.Aspire_Net_Web>("webfrontend")
     .WithExternalHttpEndpoints()
-    .WithReference(apiService);
+    .WithReference(apiService)
+    .WithReference(worker);
 
 builder.Build().Run();
